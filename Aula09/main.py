a8415b1bcbf8428f37e637e1a8f48a91dab19a5b
Nicholas Knopp Seferin da Silva <knoppnicholas4@gmail.com>
2023-05-24 10:54:22 -0300

    Aula09

---
+++ Aula09/main.py
@@ -0,0 +1,12 @@
+from Lista import Lista
+
+lista = Lista()
+lista.imprimir()
+lista.addNoFim( 10 )
+lista.addNoFim( 20 )
+lista.addNoFim( "Maria" )
+lista.addNoFim("João")
+lista.addNoFim("Abel")
+
+lista.removerDoFim()
+
--- Questao09/main.py
+++
@@ -1,5 +0,0 @@
-from AlunoGraduacao import AlunoGraduacao
-
-a = AlunoGraduacao("João", "999222200", "12345", "3 Presencas")
-a.imprimir()
-print("-------------------------------------------------")
