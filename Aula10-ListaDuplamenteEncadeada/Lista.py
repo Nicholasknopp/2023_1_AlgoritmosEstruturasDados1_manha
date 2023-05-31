b554efa43e2e4d41ee7f643c2b75e63e587432fc
Nicholas Knopp Seferin da Silva <knoppnicholas4@gmail.com>
2023-05-31 11:55:40 -0300

    Aula10-ListaDuplamenteEncadeada

--- Aula10-ListaDuplamenteEncadeada/Lista.py
+++ Aula10-ListaDuplamenteEncadeada/Lista.py
@@ -18,6 +18,19 @@ class Lista:
         self.tamanho += 1
         self.imprimir()
         
+    def addNoInicio(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+            self.fim = nodo
+        else:
+            nodo.proximo = self.inicio
+            self.inicio.anterior = nodo
+            self.inicio = nodo   
+        self.tamanho += 1
+        self.imprimir()     
+
+        
     def imprimir( self):
         print("--------------------")
         if self.inicio == None:
@@ -34,31 +47,40 @@ class Lista:
         if self.inicio == None:
             print("Lista Vazia!")
         else:
-            aux = self.inicio
+            aux = self.fim
             while aux :
                 print( aux.dado )
-                aux = aux.proximo
+                aux = aux.anterior
             print( "Tamanho: ", str(self.tamanho) )
             
             
     def removerDoFim(self):
         if self.inicio == None:
             print("Lista Vazia!")
+            return
         elif self.inicio.proximo == None:
             self.inicio = None
+            self.fim = None
             self.tamanho = 0
         else:
-            anterior = self.inicio
-            aux = self.inicio.proximo
-            while aux.proximo:
-                anterior = aux
-                aux = aux.proximo
-            anterior.proximo = None
+            self.fim = self.fim.anterior
+            self.fim.proximo = None
             self.tamanho -= 1
-            self.imprimir()
+        self.imprimir()
                 
-            
-        
+    def removerDoInicio(self):
+        if self.inicio == None:
+            print("Lista Vazia!")
+            return
+        elif self.inicio.proximo == None:
+            self.inicio = None
+            self.fim = None
+            self.tamanho = 0
+        else:
+            self.inicio = self.inicio.proximo
+            self.inicio.anterior = None
+            self.tamanho -= 1
+        self.imprimir()
         
         
         
